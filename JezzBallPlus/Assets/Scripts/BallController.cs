36cff05d07eac32f6ccc49f57a7ace4ebf21abf5
shyde8 <shyde8@gmail.com>
2019-07-07 22:20:34 -0400

    first day of work
    first day of work

---
+++ JezzBallPlus/Assets/Scripts/BallController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    //GameObject components
+    private SpriteRenderer _sprite;
+    private Rigidbody2D _body;
+
+    //sprites
+    [SerializeField]
+    private Sprite _ballSprite;
+
+    //private variables
+    [SerializeField]
+    private float speed = 10f;
+    private int[] directionArray = new int[] { -1, 1 }; //todo: add direction '0'?
+    private float collisionBoundary = 3.8f;
+
+    void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+        _body = GetComponent<Rigidbody2D>();
+
+        int randX = Random.Range(0, directionArray.Length);
+        int randY = Random.Range(0, directionArray.Length);
+        float vecX = directionArray[randX] * speed * Time.deltaTime;
+        float vecY = directionArray[randY] * speed * Time.deltaTime;
+        _body.velocity = new Vector2(vecX, vecY);        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //top-right collision point: 3.7, 4.0
+    //bottom-right collision point: 3.7, -4.0
+    //top-left: -3.7, 4.0
+    //bottom-left:
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("First point that collided: " + collision.contacts[0].point);
+        Vector2 collisionPoint = collision.contacts[0].point;
+        float xVel = _body.velocity.x;
+        float yVel = _body.velocity.y;
+        if (Mathf.Abs(collisionPoint.x) >= collisionBoundary)
+            xVel *= -1f;
+        if (Mathf.Abs(collisionPoint.y) >= collisionBoundary)
+            yVel *= -1f;
+        _body.velocity = new Vector2(xVel, yVel);         
+    }
+
+}
---
+++ JezzBallPlus/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //GameObject components
+    private SpriteRenderer _sprite;
+
+    //sprites
+    [SerializeField]
+    private Sprite _playerSprite;
+
+    //region private variables
+    [SerializeField]
+    private float _scrollSensitivity = 1f;
+    private float _clickRotation = 45f;
+    private RotationType _rotationType;
+
+    void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+        _rotationType = RotationType.Click;
+    }
+
+
+    void Update()
+    {
+        //move character
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = new Vector3(worldPoint.x, worldPoint.y, transform.position.z);
+
+        //_rotationType Scroll
+        if (_rotationType == RotationType.Scroll)
+        {
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            Vector3 zRotation = new Vector3(0, 0, scroll * _scrollSensitivity);
+            transform.Rotate(zRotation * Time.deltaTime, Space.Self);
+        }
+
+        //_rotationType Click
+        if (_rotationType == RotationType.Click)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Vector3 currentEul = transform.eulerAngles;
+                currentEul.z += _clickRotation;
+                transform.eulerAngles = currentEul;
+            }
+        }
+    }
+}
