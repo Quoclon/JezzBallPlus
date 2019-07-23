998a57ca53c5624dbad15fb5c7a3e337021a936b
shyde8 <shyde8@gmail.com>
2019-07-22 22:23:59 -0400

    calculate mintile and maxtile, plus raycast to adjacent tile
    calculate mintile and maxtile, plus raycast to adjacent tile

--- JezzBallPlus/Assets/Scripts/GridController.cs
+++ JezzBallPlus/Assets/Scripts/GridController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GridController : MonoBehaviour
@@ -20,6 +21,8 @@ public class GridController : MonoBehaviour
     private float _offsetX = 0.1f;
     private float _offsetY = 0.1f;
 
+    private ContactFilter2D contactFilter;
+
     void Start()
     {
         for (int i = 0; i < startingArray.GetLength(0); i++)
@@ -35,6 +38,10 @@ public class GridController : MonoBehaviour
             }
         }
         ResetTileCounts();
+
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(8));
+        contactFilter.useLayerMask = true;
     }
 
     // Update is called once per frame
@@ -48,6 +55,7 @@ public class GridController : MonoBehaviour
         Renderer rend = wall.GetComponent<Renderer>();
         BoxCollider2D coll = wall.GetComponent<BoxCollider2D>();
         GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        List<GameObject> newTiles = new List<GameObject>();
 
         foreach (GameObject tile in tiles)
         {
@@ -63,10 +71,73 @@ public class GridController : MonoBehaviour
                     block.transform.position = pos;
                     block.transform.SetParent(transform, true);
                     block.layer = 8;
+                    newTiles.Add(block);
                 }
             }
         }
+
+        //get direction of wall
+        //if z-rotation is 0 or 180 it is a vertical wall
+        //if z-rotation is 90 or 270 it is a horizontal wall
+        if (wall.transform.rotation.z == 0 || wall.transform.rotation.z == 180)
+        {
+            //vertical wall: find tiles with far-right x / middle y and far - left x / middle y
+            float minX = newTiles.OrderBy(t => t.transform.position.x).First().transform.position.x;
+            float maxX = newTiles.OrderByDescending(t => t.transform.position.x).First().transform.position.x;
+            float y = GetMedianYCoordinate(newTiles);
+            GameObject minTile = newTiles.Where(x => x.transform.position.x == minX).Where(z => z.transform.position.y == y).First();
+            GameObject maxTile = newTiles.Where(x => x.transform.position.x == maxX).Where(z => z.transform.position.y == y).First();
+            //raycast to the left of minTile
+            RaycastHit2D minRay = Physics2D.Raycast(minTile.transform.position, Vector2.left, _offsetX, 1 << LayerMask.NameToLayer("Grid"));
+            //Vector2 forward = transform.TransformDirection(Vector2.left) * 0.1f;
+            //Debug.DrawRay(minTile.transform.position, forward, Color.red, 100);
+            //raycast to the right of maxTile
+            RaycastHit2D maxRay = Physics2D.Raycast(maxTile.transform.position, Vector2.right, _offsetX, 1 << LayerMask.NameToLayer("Grid"));
+
+            if (minRay.collider != null)
+            {
+                if (minRay.collider.gameObject != null)
+                    FloodFill(minRay.collider.gameObject);
+            }
+            if (maxRay.collider != null)
+            {
+                if (maxRay.collider.gameObject != null)
+                    FloodFill(maxRay.collider.gameObject);
+            }
+        }
+        else
+        {
+            //horizontal wall: find tiles with middle x/top y and middle x/top y
+            float x = GetMedianXCoordinate(newTiles);
+            float minY = newTiles.OrderBy(t => t.transform.position.y).First().transform.position.y;
+            float maxY = newTiles.OrderByDescending(t => t.transform.position.y).First().transform.position.y;
+            GameObject minTile = newTiles.Where(t => t.transform.position.x == x).Where(z => z.transform.position.y == minY).First();
+            GameObject maxTile = newTiles.Where(t => t.transform.position.x == x).Where(z => z.transform.position.y == maxY).First();
+            //raycast to the bottom of minTile
+            RaycastHit2D minRay = Physics2D.Raycast(minTile.transform.position, Vector2.down, _offsetX, 1 << LayerMask.NameToLayer("Grid"));
+            //raycast to the top of maxTile
+            RaycastHit2D maxRay = Physics2D.Raycast(maxTile.transform.position, Vector2.up, _offsetX, 1 << LayerMask.NameToLayer("Grid"));
+
+
+            if (minRay.collider != null)
+            {
+                if (minRay.collider.gameObject != null)
+                    FloodFill(minRay.collider.gameObject);
+            }
+            if (maxRay.collider != null)
+            {
+                if (maxRay.collider.gameObject != null)
+                    FloodFill(maxRay.collider.gameObject);
+            }
+        }
+
+
         ResetTileCounts();
+    }
+
+    private void FloodFill(GameObject tile)
+    {
+        Debug.Log("Flood filling for: " + tile.transform.position);
     }
 
     private void ResetTileCounts()
@@ -74,7 +145,7 @@ public class GridController : MonoBehaviour
         _startingColorCount = 0;
         _targetColorCount = 0;
         GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
-        foreach(GameObject tile in tiles)
+        foreach (GameObject tile in tiles)
         {
             if (!tile.name.Contains("Black"))
                 _startingColorCount++;
@@ -84,6 +155,40 @@ public class GridController : MonoBehaviour
         Debug.Log("Black Tile Count: " + _targetColorCount);
         Debug.Log("Cyan Tile Count: " + _startingColorCount);
     }
+
+    private float GetMedianXCoordinate(List<GameObject> objects)
+    {
+        float median = 0;
+        int cnt = 0;
+        List<float> numbers = new List<float>();
+        foreach (GameObject obj in objects)
+        {
+            numbers.Add(obj.transform.position.x);
+            cnt++;
+        }
+        numbers.Sort();
+        int halfIndex = numbers.Count / 2;
+        median = numbers[halfIndex];
+        return median;
+    }
+
+    private float GetMedianYCoordinate(List<GameObject> objects)
+    {
+        float median = 0;
+        int cnt = 0;
+        List<float> numbers = new List<float>();
+        foreach (GameObject obj in objects)
+        {
+            numbers.Add(obj.transform.position.y);
+            cnt++;
+        }
+        numbers.Sort();
+        int halfIndex = numbers.Count / 2;
+        median = numbers[halfIndex];
+        return median;
+    }
+
+
 
 
 }
